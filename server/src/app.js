a552b743562dd7b931872381c2088efe9a69b83a
Jorge Contreras <jorge.contreras789@gmail.com>
2021-05-01 16:07:11 -0400

    Added ability to stream data from kepler planets csv

---
+++ server/src/app.js
@@ -0,0 +1,16 @@
+const cors = require("cors");
+const express = require("express");
+
+const planetsRouter = require("./routes/planets/planets.router");
+
+const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
+app.use(express.json());
+app.use(planetsRouter);
+
+module.exports = app;
