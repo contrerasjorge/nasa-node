a04ec0d513c54c7331093a9bbcee18564ad61280
Jorge Contreras <jorge.contreras789@gmail.com>
2021-05-02 17:37:14 -0400

    Added mongoose for mongodb

--- server/src/server.js
+++ server/src/server.js
@@ -1,4 +1,7 @@
+require("dotenv").config();
+
 const http = require("http");
+const mongoose = require("mongoose");
 
 const app = require("./app");
 
@@ -6,9 +9,26 @@ const { loadPlanetsData } = require("./models/planets.model");
 
 const PORT = process.env.PORT || 8000;
 
+const MONGO_URL = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.rqr3f.mongodb.net/nasa?retryWrites=true&w=majority`;
+
 const server = http.createServer(app);
 
+mongoose.connection.once("open", () => {
+  console.log("MongoDB connection ready! 😂 ");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.error(err);
+});
+
 async function startServer() {
+  await mongoose.connect(MONGO_URL, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  });
+
   await loadPlanetsData();
 
   server.listen(PORT, () => {
