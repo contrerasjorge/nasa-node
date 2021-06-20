5812ed93928170fabe277432a78477975ca8b74a
Jorge Contreras <jorge.contreras789@gmail.com>
2021-06-20 17:14:03 -0400

    Moved the MONGO_URL to env file

--- server/src/services/mongo.js
+++ server/src/services/mongo.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 const mongoose = require("mongoose");
 
-const MONGO_URL = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.rqr3f.mongodb.net/nasa?retryWrites=true&w=majority`;
+const MONGO_URL = process.env.MONGO_URL;
 
 mongoose.connection.once("open", () => {
   console.log("MongoDB connection ready! 😂 ");
